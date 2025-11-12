e1734ce92c8cc04a702d56d7829e682fba912835
Thabet <bali.thabet@gmail.com>
2025-11-12 17:00:16 +0100

    Blue - Refactoring

--- src/rating.py
+++ src/rating.py
@@ -1,6 +1,4 @@
 def rate_note(param):
-    if param == 8:
-        return "unsuccessful"
-    if param == 9:
+    if param == 8 or param == 9:
         return "unsuccessful"
     return "acceptable"
