bc6572c67835e1afd9835b924a7f2040e9850f3d
Thabet <bali.thabet@gmail.com>
2025-11-12 19:43:42 +0100

    blue - refactoring

--- src/rating.py
+++ src/rating.py
@@ -1,6 +1,6 @@
 def rate_note(note):
     if note < 10:
         return "unsuccessful"
-    if note == 13:
-        return "good"
-    return "acceptable"
+    if 10 <= note < 13:
+        return "acceptable"
+    return "good"
